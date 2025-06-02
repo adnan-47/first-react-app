9e92afed7e9b084e42922a2af35bbe13359a33ae
Adnan <adnanofficial47@gmail.com>
2025-06-03 00:59:59 +0530

    create the nav header hero product and pricing section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,55 +1,30 @@
-const pricingTiers = [
-  {
-    title: "Free",
-    monthlyPrice: 0,
-    buttonText: "Get started for free",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "2GB storage",
-      "Integrations",
-      "Basic support",
-    ],
-  },
-  {
-    title: "Pro",
-    monthlyPrice: 9,
-    buttonText: "Sign up now",
-    popular: true,
-    inverse: true,
-    features: [
-      "Up to 50 project members",
-      "Unlimited tasks and projects",
-      "50GB storage",
-      "Integrations",
-      "Priority support",
-      "Advanced support",
-      "Export support",
-    ],
-  },
-  {
-    title: "Business",
-    monthlyPrice: 19,
-    buttonText: "Sign up now",
-    popular: false,
-    inverse: false,
-    features: [
-      "Up to 5 project members",
-      "Unlimited tasks and projects",
-      "200GB storage",
-      "Integrations",
-      "Dedicated account manager",
-      "Custom fields",
-      "Advanced analytics",
-      "Export capabilities",
-      "API access",
-      "Advanced security features",
-    ],
-  },
-];
+
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+    <div className="container">
+      <div className="section-heading">
+      <h2 className="section-title">How to use</h2>
+      <p className="section-description mt-5">Clone the git repo below and add the files from the folder terminal into your computer</p>
+</div>
+
+  <div className="flex items-center justify-center mt-10">
+  <div className="bg-white text-black border border-blue-900 rounded-lg p-4 lg:pb-10 w-[90%] sm:w-[300px] md:w-[600px] lg:w-[900px]  shadow-md">
+    <div className="flex space-x-2 mb-3">
+      <span className="w-3 h-3 bg-red-500 rounded-full"></span>
+      <span className="w-3 h-3 bg-yellow-400 rounded-full"></span>
+      <span className="w-3 h-3 bg-green-500 rounded-full"></span>
+    </div>
+    <code className="text-green-600">$</code>{' '}
+   <code className="text-black break-all text-sm md:text-lg lg:text-[25px] md:block md:text-center">
+  git clone https://github.com/adnan-47/dotfiles.git
+</code>
+  </div>
+</div>
+</div>
+
+
+  </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,31 @@
+import product from '@/assets/product.png';
+import toollogo from '@/assets/toollogo.png';
+import webdevtool from '@/assets/webdevtool.png';
+import Image from 'next/image';
+
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-24 overflow-x-clip'>
+      <div className="container">
+        <div className='section-heading'>
+        <div className='flex justify-center'>
+        <div className='tag'>Boost your productivity</div>
+        </div>
+        <h2 className='section-title mt-5'>
+          The next level look for your powershell terminal</h2>
+        <p className='section-description mt-5'>Effortlessly turn your powershell terminal into a beautiful
+          one.Boost yur productivity by using history function </p>
+          </div>
+          <div className='relative'>
+          <Image src={product} alt='Product Image' className='mt-10' />
+          <Image src={toollogo} alt='Tool Logo' height={262} width={262} className='hidden md:block absolute -right-36 -top-32'/>  
+          <Image src={webdevtool} alt='WebDev Tool' height={100} width={100}  className='hidden md:block  absolute bottom-6 -left-[60px] lg:-left-[150px] lg:-bottom-8 lg:w-[200px] '/>
+          </div>
+      </div>
+    </section>
+
+
+
+  );
 };
